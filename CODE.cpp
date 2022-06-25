e4b1c21bacccc6401429ba68bcf09b647add4574
sanjeevganji <85863002+sanjeevganji@users.noreply.github.com>
2022-06-25 13:00:34 +0530

    Add files via upload

---
+++ CODE.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+//#include <climits>
+//#include <cmath>
+//#include <string.h>
+#define endl '\n'
+//#define PB push_back 
+//#define pB pop_back 	
+//#define pF pop_front 
+//#define PF push_front 
+//#define mod 1000000007 
+//#define luup(i,a,b) for(int i=a;i<b;i++)
+//typedef long long ll;
+//typedef long double ld; 
+
+using namespace std;
+
+
+int main(){
+
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+cout
+
+
+return 0;
+}
